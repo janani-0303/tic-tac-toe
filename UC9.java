6ab465a2d0ad3413b3b1469a8b71e955d68fa9d8
janani-0303 <jt9271@srmist.edu.in>
2026-05-05 16:59:04 +0530

    Coded Check winning condition

---
+++ UC9.java
@@ -0,0 +1,45 @@
+public class UC9{
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        System.out.println(hasWon('X'));
+    }
+
+    static boolean hasWon(char symbol) {
+
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol &&
+                board[i][1] == symbol &&
+                board[i][2] == symbol) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == symbol &&
+                board[1][j] == symbol &&
+                board[2][j] == symbol) {
+                return true;
+            }
+        }
+
+        // Check main diagonal
+        if (board[0][0] == symbol &&
+            board[1][1] == symbol &&
+            board[2][2] == symbol) {
+            return true;
+        }
+
+        // Check anti-diagonal
+        if (board[0][2] == symbol &&
+            board[1][1] == symbol &&
+            board[2][0] == symbol) {
+            return true;
+        }
+
+        return false;
+    }
+}
